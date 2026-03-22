d3b2318a5c017078ea86d294e4ae755a34db773e
Sofiya <imanlysofi@gmail.com>
2026-03-22 17:58:35 +0000

    Add task 12 with custom return logic

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-
-def fizzbuzz_logic(number):
-    if number % 3 == 0 and number % 5 == 0:
-        return 'FizzBuzz'
-    elif number % 3 == 0:
-        return 'Fizz'
-    elif number % 5 == 0:
-        return 'Buzz'
-    else:
-        return number
-
 def fizzbuzz():
     for number in range(1, 101):
-        result = fizzbuzz_logic(number)�ен
-        if number < 100:
-            print("{}".format(result), end=" ")
+        if number % 3 == 0 and number % 5 == 0:
+            res = "FizzBuzz"
+        elif number % 3 == 0:
+            res = "Fizz"
+        elif number % 5 == 0:
+            res = "Buzz"
         else:
-            print("{}".format(result))
+            res = number
+
+        if number < 100:
+            print("{}".format(res), end=" ")
+        else:
+            print("{}".format(res))
