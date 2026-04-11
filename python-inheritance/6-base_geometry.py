b3c6b6cf246bcc88db875ae853f6625b6591c531
Sofiya <imanlysofi@gmail.com>
2026-04-11 17:45:03 +0000

    Add Task 6: Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry based on 5-base_geometry.py.
+"""
+
+
+class BaseGeometry:
+    """A class with a public instance method area."""
+
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
