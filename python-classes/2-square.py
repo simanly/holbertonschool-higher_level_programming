4984b95a67251b7aa8b3de1b6295d37c6165ade7
Sofiya <imanlysofi@gmail.com>
2026-04-11 13:55:38 +0000

    Add Task 2:  Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with size validation.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
