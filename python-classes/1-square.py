a6b4bbc7d9c7fe365e5867f35193377d288fdf0f
Sofiya <imanlysofi@gmail.com>
2026-04-11 11:49:31 +0000

    Add task 1:

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module that defines
+Square class
+'''
+class Square:
+    '''
+    A class that represents a square
+    '''
+    def __init__(self, size):
+        '''
+        Initializes the square data
+        '''
+        self.__size = size
