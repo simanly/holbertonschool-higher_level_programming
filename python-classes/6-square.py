fa667358682c0a6119518af4c3ee68e7c6c8c5e4
Sofiya <imanlysofi@gmail.com>
2026-04-11 16:32:07 +0000

    Fix docs and logic

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""Module that defines a Square class."""
+
 
 class Square:
+    """Class representing a square."""
+
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square."""
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Retrieve the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -19,10 +26,12 @@ class Square:
 
     @property
     def position(self):
+        """Retrieve the position."""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """Set the position with validation."""
         if (not isinstance(value, tuple) or len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
@@ -30,9 +39,11 @@ class Square:
         self.__position = value
 
     def area(self):
+        """Return the current area."""
         return self.__size ** 2
 
     def my_print(self):
+        """Print the square with # and handle position."""
         if self.__size == 0:
             print("")
             return
